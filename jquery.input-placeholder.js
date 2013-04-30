93d7caa217a37cd00b47a3f89cea2bf75a663faf
yohannrub <yohannrub@users.noreply.github.com>
2013-04-30 14:02:49 +0200

    refactor code

--- jquery.input-placeholder.js
+++ jquery.input-placeholder.js
@@ -1,35 +1,36 @@
 /*!
- * jQuery Input Placeholder v@1.0
+ * jQuery Input Placeholder v@1.1
  * https://github.com/yohannrub/jquery.input-placeholder
  * Licensed under the MIT license
  */
 
 ;(function($) {
 
-    var PLACEHOLDER_CLASS = 'placeholder';
+    var PLACEHOLDER_CLASS = 'placeholder',
+        PLACEHOLDER_ATTRIBUTE = 'placeholder',
+        PLACEHOLDER_SELECTOR = '[placeholder]';
 
-    $.support.placeholder = ('placeholder' in document.createElement('input'));
+    $.support.placeholder = (PLACEHOLDER_ATTRIBUTE in document.createElement('input'));
 
     if (!$.support.placeholder) {
-        $(document).ready(function() {
-            var $inputsAll = $('[placeholder]');
+        $(function() {
+            var $allInputs = $(PLACEHOLDER_SELECTOR);
 
-            $inputsAll.focus(function() {
+            $allInputs.focus(function() {
                 var $input = $(this);
-                if ($input.val() == $input.attr('placeholder')) {
-                    $input.val('');
-                    $input.removeClass(PLACEHOLDER_CLASS);
+                if ($input.val() == $input.attr(PLACEHOLDER_ATTRIBUTE)) {
+                    $input.val('').removeClass(PLACEHOLDER_CLASS);
                 }
             }).blur(function() {
-                var $input = $(this);
-                if ($input.val() == '' || $input.val() == $input.attr('placeholder')) {
-                    $input.addClass(PLACEHOLDER_CLASS);
-                    $input.val($input.attr('placeholder'));
+                var $input = $(this),
+                    inputVal = $input.val();
+                if (inputVal == '' || inputVal == $input.attr(PLACEHOLDER_ATTRIBUTE)) {
+                    $input.val($input.attr(PLACEHOLDER_ATTRIBUTE)).addClass(PLACEHOLDER_CLASS);
                 }
             });
 
-            $inputsAll.parents('form').on('submit', function() {
-                var $inputs = $(this).find('[placeholder]');
+            $allInputs.parents('form').on('submit', function() {
+                var $inputs = $(this).find(PLACEHOLDER_SELECTOR);
                 $inputs.each(function() {
                     $(this).triggerHandler('focus');
                 });
@@ -39,18 +40,17 @@
                     });
                 }, 50);
             }).on('reset', function() {
-                var $inputs = $(this).find('[placeholder]');
+                var $inputs = $(this).find(PLACEHOLDER_SELECTOR);
                 setTimeout(function() {
                     $inputs.each(function() {
                         var $input = $(this);
-                        $input.addClass(PLACEHOLDER_CLASS);
-                        $input.val($input.attr('placeholder'));
+                        $input.val($input.attr(PLACEHOLDER_ATTRIBUTE)).addClass(PLACEHOLDER_CLASS);
                     });
                 }, 0);
             });
 
             setTimeout(function() {
-                $inputsAll.each(function() {
+                $allInputs.each(function() {
                     $(this).triggerHandler('blur');
                 });
             }, 0);
